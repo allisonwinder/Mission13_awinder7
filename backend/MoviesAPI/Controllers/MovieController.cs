a089d65fd7c1bb47fb86a6e67e23bbc77eea9ea7
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2023-04-05 09:09:04 -0600

    api set up

---
+++ backend/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private MoviesDbContext context;
+
+        public MovieController(MoviesDbContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            return context.Movies.ToArray();
+        }
+    }
+}
